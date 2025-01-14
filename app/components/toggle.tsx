b2681a052e51a748226627060dcd1a1695a5da1d
OlatunjiAbiodun <olatunjia889@gmail.com>
2025-01-14 07:19:29 +0100

    add hero section'

--- app/components/toggle.tsx
+++ app/components/toggle.tsx
@@ -24,7 +24,7 @@ export function ModeToggle() {
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="text-[#99e570]">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
